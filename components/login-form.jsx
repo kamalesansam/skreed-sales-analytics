0e2e687c8f41fb192be6ec0b33ff90a61c85b424
kamalesansam <sam.kamalesan_2027@woxsen.edu.in>
2026-05-22 12:46:11 +0530

    style: remove dark mode brightness and grayscale filters from login image

--- components/login-form.jsx
+++ components/login-form.jsx
@@ -79,7 +79,7 @@ export function LoginForm({
             <img
               src="/assets/login.png"
               alt="Image"
-              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale" />
+              className="absolute inset-0 h-full w-full object-cover" />
           </div>
         </CardContent>
       </Card>
